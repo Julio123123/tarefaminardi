6fec40b9b7d016383bf1bcbb2ff802d426720d96
Julio123123 <119547834+Julio123123@users.noreply.github.com>
2023-11-08 20:26:35 -0300

    Add files via upload

--- Aluguelcarros/database/alugados.sql
+++ Aluguelcarros/database/alugados.sql
@@ -13,6 +13,12 @@ CREATE TABLE carros (
     documento VARCHAR(40) NOT NULL,
     marca VARCHAR(20) NOT NULL,
     modelo VARCHAR(20) NOT NULL,
-    
+    km_rodado DECIMAL(9,3) NOT NULL,
+    cor VARCHAR(20) NOT NULL,
+    link VARCHAR(255) NOT NULL,
+    combustivel VARCHAR(20) NOT NULL,
+    media DECIMAL(5,3) NOT NULL,
+    transmissao VARCHAR(20) NOT NULL,
+
     PRIMARY KEY (id)
-); 
+);
